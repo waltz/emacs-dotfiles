a9d83b7390512757c6305371b0e26679d9c20cbb
Christian Bryan <christianbryan@gmail.com>
2012-06-26 14:27:12 -0400

    Remove a handful of the starter kit modules and see how it goes.

--- init.el
+++ init.el
@@ -11,56 +11,56 @@
 
 ;; Turn off mouse interface early in startup to avoid momentary display
 ;; You really don't need these; trust me.
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; Load path etc.
 
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
+;; (setq dotfiles-dir (file-name-directory
+;;                     (or (buffer-file-name) load-file-name)))
 
 ;; Load up ELPA, the package manager
 
-(add-to-list 'load-path dotfiles-dir)
+;; (add-to-list 'load-path dotfiles-dir)
 
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+;; (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
 
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq package-user-dir (concat dotfiles-dir "elpa"))
-(setq custom-file (concat dotfiles-dir "custom.el"))
+;; (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+;; (setq package-user-dir (concat dotfiles-dir "elpa"))
+;; (setq custom-file (concat dotfiles-dir "custom.el"))
 
-(require 'package)
-(package-initialize)
-(require 'starter-kit-elpa)
+;; (require 'package)
+;; (package-initialize)
+;; (require 'starter-kit-elpa)
 
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
 
-(require 'cl)
-(require 'saveplace)
-(require 'ffap)
-(require 'uniquify)
-(require 'ansi-color)
-(require 'recentf)
+;; (require 'cl)
+;; (require 'saveplace)
+;; (require 'ffap)
+;; (require 'uniquify)
+;; (require 'ansi-color)
+;; (require 'recentf)
 
 ;; backport some functionality to Emacs 22 if needed
-(require 'dominating-file)
+;; (require 'dominating-file)
 
 ;; Load up starter kit customizations
 
-(require 'starter-kit-defuns)
-(require 'starter-kit-bindings)
-(require 'starter-kit-misc)
-(require 'starter-kit-registers)
-(require 'starter-kit-eshell)
-(require 'starter-kit-lisp)
-(require 'starter-kit-perl)
-(require 'starter-kit-ruby)
+;; (require 'starter-kit-defuns)
+;; (require 'starter-kit-bindings)
+;; (require 'starter-kit-misc)
+;; (require 'starter-kit-registers)
+;; (require 'starter-kit-eshell)
+;; (require 'starter-kit-lisp)
+;; (require 'starter-kit-perl)
+;; (require 'starter-kit-ruby)
 ;; (require 'starter-kit-js)
 
-(regen-autoloads)
-(load custom-file 'noerror)
+;; (regen-autoloads)
+;; (load custom-file 'noerror)
 
 ;; Dear lord, indentation preferences!
 (setq-default indent-tabs-mode nil)
